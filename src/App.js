1355dd35f598903528c3110f86fcb9bb19d08d91
yaswantharao05 <yaswanthaparagada@gmail.com>
2023-10-14 21:15:45 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import F1 from './components/F1'
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+  const colors = ["#fe0000","#00ff01","#0000fe","#ffff00","#ff00fe","#00ffff","#fea500","#81007f",
+                  "#ffc0cb","#008001","#fe6347","#00cfd2","#8b4511","#ff8b00","#4682b4","#fed700"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <F1 colors={colors}/>
     </div>
   );
 }
---
+++ src/components/F1.js
@@ -0,0 +1,77 @@
+
+
+// function F1(props){
+//     // constructor(props){
+//     //     super(props);
+//     //     this.state = {bool: true};
+//     // }
+//     console.log(props);
+//     // const boxcolor = {
+
+//     // }
+//     return(
+        // <div id="grad">
+        //     <h1 class="heading">Color Picker</h1>
+        //     <div class="colorBox">
+
+        //     </div>
+        // </div>
+//     )
+// }
+
+// export default F1;
+
+import React, { Component } from 'react';
+import './style.css'
+
+class F1 extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: false,
+      selectedColor: "#90EE90",
+    };
+  }
+
+  toggleColorList = () => {
+    this.setState((prevState) => ({
+      isOpen: !prevState.isOpen,
+    }));
+  };
+
+  handleColorSelect = (color) => {
+    this.setState({
+      selectedColor: color,
+      isOpen: false,
+    });
+  };
+
+  render() {
+    const { colors } = this.props;
+    const { isOpen, selectedColor } = this.state;
+
+    return (
+        <div id="grad">
+            <h1 class="heading">Color Picker</h1>
+            <div class="colorBox" style={{backgroundColor: selectedColor}}>
+                {isOpen && (
+                <div className="color-list">
+                    {colors.map((color, index) => (
+                    <button
+                        key={index}
+                        className="color-option"
+                        style={{ backgroundColor: color }}
+                        onClick={() => this.handleColorSelect(color)}
+                    ></button>
+                    ))}
+                </div>
+                )}
+            </div>
+            <button className="pickColor btn btn-dark" onClick={this.toggleColorList}>Pick a color</button>
+        </div>
+    );
+  }
+}
+
+export default F1;
